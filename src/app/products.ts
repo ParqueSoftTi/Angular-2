187b43ed011f3ac17d09ac30b0ae829745f886c7
ParqueSoftTi <167160425+ParqueSoftTi@users.noreply.github.com>
2024-05-23 18:43:00 -0500

    Add files via upload

---
+++ src/app/products.ts
@@ -0,0 +1,54 @@
+export interface Product {
+  id: number;
+  name: string;
+  price: number;
+  description: string;
+}
+
+export const products = [
+  {
+    id: 1,
+    name: 'Phone XL',
+    price: 799,
+    description: 'Un teléfono grande con una de las mejores pantallas'
+  },
+  {
+    id: 2,
+    name: 'Phone Mini',
+    price: 699,
+    description: 'Un gran teléfono con una de las mejores cámaras'
+  },
+  {
+    id: 3,
+    name: 'Pixel 4a',
+    price: 500,
+    description: ''
+  },
+  {
+    id: 4,
+    name: 'Pixel 5',
+    price: 700,
+    description: 'Flafship de Google a un precio asequible'
+  },
+  {
+    id: 5,
+    name: 'Pixel 5 XL',
+    price: 800,
+    description: 'Flafship de Google a un precio asequible' 
+  },
+  {
+    id : 6,
+    name: 'Pixel 6',
+    price: 900,
+    description: 'La mejor expereincia de Android'
+  },
+  {
+    id: 7,
+    name: 'Pixel 6 XL',
+    price: 1000,
+    description: 'La mejor expereincia de Android en un teléfono grande'
+  }
+  
+];
+
+
